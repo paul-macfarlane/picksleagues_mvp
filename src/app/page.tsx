862fc4b1b4e8c200f6a8882cbe4d22bffe7c0bae
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-09-04 18:36:38 -0600

    add initial database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-export default function Home() {
+import { db } from "@/db/client";
+import { countries } from "@/db/schema";
+
+export default async function Home() {
+  const countryList = await db.select().from(countries);
+
   return (
     <main>
       <h1>Hello Pix</h1>
+
+      {countryList.length > 0 && (
+        <ul>
+          {countryList.map((country) => (
+            <li key={country.id}>{country.name}</li>
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
---
+++ src/db/client.ts
@@ -0,0 +1,18 @@
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+
+// see https://github.com/tursodatabase/libsql-client-ts/issues/225
+function noCacheFetch(
+  input: string | URL | globalThis.Request,
+  init?: RequestInit
+): Promise<Response> {
+  return fetch(input, { ...init, cache: "no-store" });
+}
+
+const client = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN!,
+  fetch: noCacheFetch,
+});
+
+export const db = drizzle(client);
