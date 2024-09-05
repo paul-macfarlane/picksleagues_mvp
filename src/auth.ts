d176fb2dbeb3981fb6bbe989c115e60ff54b2c96
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2024-09-05 16:55:15 -0600

    add db integration with auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,14 @@
 import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import Google from "next-auth/providers/google";
+import { db } from "./db/client";
+import { accounts, sessions, users } from "./db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+  }),
   providers: [Google],
 });
