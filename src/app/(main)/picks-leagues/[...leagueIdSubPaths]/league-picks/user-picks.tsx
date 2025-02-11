9bcdfd2413f6f336776ed95c506399f3945430c8
Paul Macfarlane <pauljosemacfarlane@gmail.com>
2025-02-11 12:38:14 -0500

    league picks card improvements

--- src/app/(main)/picks-leagues/[...leagueIdSubPaths]/league-picks/user-picks.tsx
+++ src/app/(main)/picks-leagues/[...leagueIdSubPaths]/league-picks/user-picks.tsx
@@ -12,6 +12,7 @@ import { PicksLeagueGameBox } from "@/app/(main)/picks-leagues/[...leagueIdSubPa
 import { DBWeeklyPickDataByUser } from "@/db/sportLeagueWeeks";
 import { PicksLeaguePickTypes } from "@/models/picksLeagues";
 import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
 
 export interface UserPicksProps {
   data: DBWeeklyPickDataByUser & {
@@ -24,6 +25,8 @@ export interface UserPicksProps {
 }
 
 export function UserPicks({ data, pickType, oddEven }: UserPicksProps) {
+  // todo appearance is good, make some changes so that there is gold silver and bronze
+
   const [picksOpen, setPicksOpen] = useState(false);
 
   const { pointsEarned, pointsRemaining } =
@@ -34,39 +37,43 @@ export function UserPicks({ data, pickType, oddEven }: UserPicksProps) {
       key={data.id}
       className={`flex flex-col space-y-2 rounded border ${oddEven === "odd" ? "bg-muted/30" : "bg-card"}`}
     >
-      <div className="flex flex-col gap-4 p-4 md:flex-row md:items-center md:justify-between">
-        <div className="flex items-center gap-4">
-          <div className="flex flex-col items-center gap-1 text-center">
-            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary/10">
-              <span className="text-sm font-semibold text-primary">
-                #{data.weekRank}
-              </span>
-            </div>
-            <span className="text-xs text-muted-foreground">Week</span>
-          </div>
-          <div className="flex flex-col items-center gap-1 text-center">
-            <div className="flex h-8 w-8 items-center justify-center rounded-full bg-success/10">
-              <span className="text-sm font-semibold text-success">
-                #{data.seasonRank}
-              </span>
-            </div>
-            <span className="text-xs text-muted-foreground">Season</span>
-          </div>
-          <div className="flex items-center gap-2">
-            <Avatar className="h-10 w-10">
-              <AvatarImage src={data.image ?? undefined} alt={data.username!} />
-              <AvatarFallback>
-                {data.username!.charAt(0).toLocaleUpperCase()}
-              </AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col">
-              <span className="font-semibold">{data.username}</span>
-              <span className="text-sm text-muted-foreground">{`${data.firstName} ${data.lastName}`}</span>
-            </div>
+      <div className="flex flex-col gap-4 p-4">
+        <div className="flex items-center gap-3">
+          <Avatar className="h-10 w-10">
+            <AvatarImage src={data.image ?? undefined} alt={data.username!} />
+            <AvatarFallback>
+              {data.username!.charAt(0).toLocaleUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
+            <p className="text-wrap font-semibold">{data.username}</p>
+            <span className="text-sm text-muted-foreground">{`${data.firstName} ${data.lastName}`}</span>
           </div>
         </div>
 
-        <div className="flex flex-col items-start gap-2 md:flex-row md:items-center md:gap-4">
+        <Separator />
+
+        <div className="flex flex-wrap items-center justify-between gap-4">
+          <div className="flex items-center gap-4">
+            <div className="flex flex-col items-center gap-1 text-center">
+              <div className="flex h-8 w-8 items-center justify-center rounded-full bg-primary/10">
+                <span className="text-sm font-semibold text-primary">
+                  #{data.weekRank}
+                </span>
+              </div>
+              <span className="text-xs text-muted-foreground">Week</span>
+            </div>
+            <div className="flex flex-col items-center gap-1 text-center">
+              <div className="flex h-8 w-8 items-center justify-center rounded-full bg-success/10">
+                <span className="text-sm font-semibold text-success">
+                  #{data.seasonRank}
+                </span>
+              </div>
+              <span className="text-xs text-muted-foreground">Season</span>
+            </div>
+          </div>
+
+          {/* Points */}
           <div className="flex flex-col items-end gap-1">
             <div className="flex items-center gap-2">
               <span
@@ -87,9 +94,9 @@ export function UserPicks({ data, pickType, oddEven }: UserPicksProps) {
         </div>
       </div>
 
-      <Collapsible className="!m-0 flex flex-col !p-0">
+      <Collapsible open={picksOpen}>
         <CollapsibleTrigger
-          className="mx-4 mb-4 flex items-center gap-2 self-end rounded-lg border px-4 py-2 hover:bg-accent"
+          className="flex w-full items-center justify-between bg-secondary p-4 text-secondary-foreground hover:bg-secondary/80"
           onClick={() => setPicksOpen(!picksOpen)}
         >
           <span className="text-sm font-medium">
